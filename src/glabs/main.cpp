42c11b244ddd41e5160836c89580990ab3acad9c
Leonid Ovsyannikov <leo2004ovs@gmail.com>
2024-09-07 17:49:21 +0300

    Add GLEW
    Add glew 2.2 source as an external library. Test OpenGL initialization
    in main.

--- src/glabs/main.cpp
+++ src/glabs/main.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main()
 {
-	int result = glfwInit();
-	if (result != GLFW_TRUE)
+	if (glfwInit() != GLFW_TRUE)
 	{
 		std::cerr << "failed to initialize glfw\n";
 		return 1;
 	}
 
 	GLFWwindow* wnd = glfwCreateWindow(1280, 720, "Hello world", nullptr, nullptr);
+	glfwMakeContextCurrent(wnd);
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "failed to initialize glew\n";
+		return 2;
+	}
+
+	std::cout << "OpenGL Version: " <<  glGetString(GL_VERSION) << '\n';
 
 	while (!glfwWindowShouldClose(wnd))
 	{
 		glfwPollEvents();
+		glfwSwapBuffers(wnd);
 	}
 
 	glfwDestroyWindow(wnd);
