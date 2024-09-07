5df59bba33010bae1d6d97b14347e2139c9bb241
Leonid Ovsyannikov <leo2004ovs@gmail.com>
2024-09-07 17:36:34 +0300

    Add CMP and GLFW
    Add CPM as package manager and add GLFW3 as a CPM dependency.

--- src/glabs/main.cpp
+++ src/glabs/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
 int main()
 {
-	std::cout << "Hello world";
+	int result = glfwInit();
+	if (result != GLFW_TRUE)
+	{
+		std::cerr << "failed to initialize glfw\n";
+		return 1;
+	}
+
+	GLFWwindow* wnd = glfwCreateWindow(1280, 720, "Hello world", nullptr, nullptr);
+
+	while (!glfwWindowShouldClose(wnd))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(wnd);
 }
 
