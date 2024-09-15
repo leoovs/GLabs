df6d6af26d3d6812b7ebb67864e47a696f0f90dc
Leonid Ovsyannikov <leo2004ovs@gmail.com>
2024-09-15 12:02:05 +0300

    Add task enumeration

---
+++ src/glabs/application/lab_task.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace glabs
+{
+	enum class LabTaskKind
+	{
+		DrawNGon, // Задание 1-2
+		DrawFirstShape, // Задание 3
+		DrawSecondShapeLineStrip, // Задание 4
+		DrawSecondShapeTriangles, // Задание 5
+		DrawNGonTriangleStrip, // Задание 6
+		DrawThirdShape, // Задание 7-8
+		Count_,
+	};
+}
+
